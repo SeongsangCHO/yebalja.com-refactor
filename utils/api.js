73f5648c7faacdc46e467f5afebe745b41be23ea
seongsangcho <liasqui31@gmail.com>
2021-11-27 15:18:29 +0900

    modify base url - aws

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = `http://localhost:8080`;
+// const BASE_URL = `http://localhost:8080`;
+const BASE_URL = `http://test.yebalja.com`;
 const END_POINT = {
   yearlycalendar: "/api/yearlycalendar",
   timelinelist: "/api/timelinelist",
